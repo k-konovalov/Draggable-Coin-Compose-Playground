293613407157bdba8f681bd0013f8caa78346ae1
Konovalov Kirill <k.konovalov@indriver.com>
2023-03-05 18:56:32 +0200

    Package renaming

---
+++ app/src/main/java/com/konovalov/draggable/coin/view/ComposeActivity.kt
@@ -0,0 +1,43 @@
+package com.konovalov.draggable.coin.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.konovalov.draggable.coin.view.ui.theme.DraggableScaredViewTheme
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DraggableScaredViewTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DraggableScaredViewTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/konovalov/draggable/coin/view/FirstFragment.kt
@@ -0,0 +1,5 @@
+package com.konovalov.draggable.coin.view
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment(R.layout.fragment_first)
