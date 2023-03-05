d0d1a1fac4cddd8c41f5f4213108459b90c9527c
Konovalov Kirill <k.konovalov@indriver.com>
2023-03-05 18:38:48 +0200

    Import Local project as is

---
+++ app/src/main/java/com/example/draggablescaredview/FirstFragment.kt
@@ -0,0 +1,17 @@
+package com.example.draggablescaredview
+
+import android.os.Bundle
+import android.view.GestureDetector
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.SimpleAdapter
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.draggablescaredview.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment(R.layout.fragment_first)
