edf6a27a3cce987cd15610ef1929f9d101654165
Konovalov Kirill <k.konovalov@indriver.com>
2023-03-16 08:28:18 +0200

    delete WindowCompat.setDecorFitsSystemWindows cause it break composition

--- app/src/main/java/com/konovalov/draggable/coin/view/MainActivity.kt
+++ app/src/main/java/com/konovalov/draggable/coin/view/MainActivity.kt
@@ -2,7 +2,6 @@ package com.konovalov.draggable.coin.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -10,19 +9,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        super.onCreate(savedInstanceState)
-       // val navController = findNavController(R.id.nav_host_fragment_content_main)
- //       appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+    private val navController
+        get() = findNavController(R.id.nav_host_fragment_content_main)
 }
